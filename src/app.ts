97da467bee9dffc60f222d6cc935ba8cf14a7559
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-07-22 20:21:45 +0300

    add express

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
 import 'dotenv/config';
 import { Telegraf } from 'telegraf';
+import express from 'express';
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
 // Create your bot and tell it about your context type
 const bot = new Telegraf(process.env.BOT_TOKEN);
