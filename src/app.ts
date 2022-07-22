90406bbfac2a94cd82bdba69afc46c15f5297184
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-07-23 00:30:45 +0300

    add rss

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import 'dotenv/config';
 import { Telegraf } from 'telegraf';
+import Parser from 'rss-parser';
 import express from 'express';
 const app = express();
+
+const parser = new Parser();
 
 const port = process.env.PORT || 3000;
 
@@ -22,6 +25,33 @@ bot.hears('hi', (ctx) => ctx.reply('Hi'));
 
 bot.hears('dice', (ctx) => {
   ctx.replyWithDice();
+});
+
+bot.hears('eco', async (ctx) => {
+  const feed = await parser.parseURL('https://www.ntv.com.tr/ekonomi.rss');
+
+  feed.items.forEach((item) => {
+    if (new Date(item.isoDate).getTime() > Date.now() - 24 * 60 * 60 * 1000)
+      ctx.reply(item.title + ':' + item.link);
+  });
+});
+
+bot.hears('son', async (ctx) => {
+  const feed = await parser.parseURL('https://www.ntv.com.tr/son-dakika.rss');
+
+  feed.items.forEach((item) => {
+    if (new Date(item.isoDate).getTime() > Date.now() - 24 * 60 * 60 * 1000)
+      ctx.reply(item.title + ':' + item.link);
+  });
+});
+
+bot.hears('money', async (ctx) => {
+  const feed = await parser.parseURL('https://www.ntv.com.tr/ntvpara.rss');
+
+  feed.items.forEach((item) => {
+    if (new Date(item.isoDate).getTime() > Date.now() - 24 * 60 * 60 * 1000)
+      ctx.reply(item.title + ':' + item.link);
+  });
 });
 
 bot.launch();
