5affa23d46c0f97e763449e81e5093321b2b4507
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-07-23 13:20:36 +0300

    give up on cron

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import { Telegraf } from 'telegraf';
 import Parser from 'rss-parser';
 import express from 'express';
-import { CronJob } from 'cron';
 const app = express();
 
 const parser = new Parser();
@@ -12,33 +11,11 @@ const port = process.env.PORT || 3000;
 // Create your bot and tell it about your context type
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// */10 * * * *
-
-const job = new CronJob(
-  '* * * * *',
-  function () {
-    bot.hears('son', async (ctx) => {
-      const feed = await parser.parseURL(
-        'https://www.ntv.com.tr/son-dakika.rss'
-      );
-
-      feed.items.forEach((item) => {
-        if (new Date(item.isoDate).getTime() > Date.now() - 5 * 60 * 1000)
-          ctx.reply(item.title + ':' + item.link);
-      });
-    });
-  },
-  null,
-  true,
-  'Europe/Istanbul'
-);
-
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  job.start();
   console.log(`Listening on port ${port}`);
 });
 
@@ -48,6 +25,15 @@ bot.hears('hi', (ctx) => ctx.reply('Hi'));
 
 bot.hears('dice', (ctx) => {
   ctx.replyWithDice();
+});
+
+bot.hears('son', async (ctx) => {
+  const feed = await parser.parseURL('https://www.ntv.com.tr/son-dakika.rss');
+
+  feed.items.forEach((item) => {
+    if (new Date(item.isoDate).getTime() > Date.now() - 24 * 60 * 60 * 1000)
+      ctx.reply(item.title + ':' + item.link);
+  });
 });
 
 bot.hears('eco', async (ctx) => {
